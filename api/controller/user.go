206c4d84ac8d9abd20922134fec7488b698df97b
Buse Duran <buseduran500@gmail.com>
2024-07-02 19:42:38 +0300

    ✨ feat: add sign in functionality and generate JWT token

--- api/controller/user.go
+++ api/controller/user.go
@@ -2,8 +2,10 @@ package controller
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"gonote.com/api/service"
 	"gonote.com/models"
 	"gonote.com/util"
@@ -39,3 +41,34 @@ func (u *UserController) SignUp(ctx *gin.Context) {
 
 // signin user
 // generate jwt token if user logged in to the system
+func (u *UserController) SignIn(ctx *gin.Context) {
+	var user models.UserLogin
+	var SampleSecret []byte
+
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		util.ErrorJSON(ctx, http.StatusBadRequest, "Invalid Json")
+		return
+	}
+	dbUser, err := u.service.SignIn(user)
+	if err != nil {
+		util.ErrorJSON(ctx, http.StatusBadRequest, "Invalid Login")
+		return
+	}
+	//generate token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user": dbUser,
+		"exp":  time.Now().Add(time.Minute * 15).Unix(),
+	})
+
+	tokenString, err := token.SignedString(SampleSecret)
+	if err != nil {
+		util.ErrorJSON(ctx, http.StatusBadRequest, "Failed to get token")
+		return
+	}
+	response := &util.Response{
+		Success: true,
+		Message: "Token generation is successfull",
+		Data:    tokenString,
+	}
+	ctx.JSON(http.StatusOK, response)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.1
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.23.0
 	gorm.io/driver/mysql v1.5.7
