ec8d5c5b37e779c553c57b28d5fbf252ffcf3f72
Buse Duran <buseduran500@gmail.com>
2024-07-07 22:09:17 +0300

    ⚰️

--- views/js/app.jsx
+++ views/js/app.jsx
@@ -1,5 +1,3 @@
-import Home from './homec.jsx';
-
 class App extends React.Component {
     render() {
         if (this.loggedIn) {
@@ -10,8 +8,6 @@ class App extends React.Component {
     }
 }
 
-
-//note component
 const Note = ({ note }) => (
     <div className="note">
         <h3>{note.title}</h3>
@@ -20,9 +16,91 @@ const Note = ({ note }) => (
     </div>
 );
 
-//user's page
-class LoggedIn extends React.Component {
+class Home extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: []
+        };
+    }
+
+    componentDidMount() {
+        this.serverRequest();
+    }
+
+    serverRequest() {
+        fetch("http://localhost:8000/notes")
+            .then(response => response.json())
+            .then(res => {
+                if (res.success) {
+                    this.setState({
+                        notes: res.data.rows
+                    });
+                }
+            })
+            .catch(error => console.error('Error fetching notes:', error));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
+                    <h1>GoNote</h1>
+                    <p>A load of notes :3</p>
+                    <p>Sign in to take notes </p>
+                    <a onClick={this.authenticate} className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
+                </div>
+                <div className="container">
+                    <div className="col-lg-12">
+                        <br />
+                        <span className="pull-right"><a onClick={this.logout}>Log out</a></span>
+                        <h2>GoNote</h2>
+                        <p>Let's feed you with some notes</p>
+                        <div className="row">
+                            {this.state.notes.map((note, i) => (
+                                <Note key={i} note={note} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class LoggedIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: []
+        }
+    }
+    serverRequest() {
+        $.get("http://localhost:8000/notes/", res => {
+            this.setState({
+                notes: res
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="col-lg-12">
+                    <br />
+                    <span className="pull-right"><a onClick={this.logout}>Log out</a></span>
+                    <h2>GoNote</h2>
+                    <p>Let's feed you with some notes</p>
+                    <div className="row">
+                        {this.state.notes.map(function (note, i) {
+                            return (<Note key={i} note={note} />);
+                        })}
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
