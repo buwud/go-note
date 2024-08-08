b2bf2eb3824521e0f7c11428fddb7ab9914dd56b
Buse Duran <buseduran500@gmail.com>
2024-08-09 00:14:44 +0300

    ♻️ refactor: update About page styles and content

--- frontend/pages/about/About.tsx
+++ frontend/pages/about/About.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import './About.css';
+import { Link } from 'react-router-dom'
 
 export default function About() {
     return (
-        <div className="about-container">
-            <h1>About Us</h1>
-            <p>
-                Welcome to our weather app! We provide up-to-date weather information for locations around the world.
-            </p>
-            <p>
-                Our mission is to make sure you are always prepared for whatever weather comes your way. Whether you're planning a trip or just need to know if you should bring an umbrella, we've got you covered!
-            </p>
-            <p>
-                This app was created with love by a dedicated team of developers who believe in making weather information accessible and easy to use for everyone.
-            </p>
-            <footer>
-                <p>Created by buwu | Hacked by Tahinli</p>
-            </footer>
+        <div className="about-page">
+            <header className="navbar">
+                <h1 className="home-title">Go Note</h1>
+                <nav>
+                    <ul className="navbar-menu">
+                        <li><a href="/" className="navbar-link">Home</a></li>
+                        <li><a href="/about" className="navbar-link">About</a></li>
+                        <li><a href="/help" className="navbar-link">Help</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <div className="about-container">
+                <h1>About Us</h1>
+                <p>This is the About page of our website. Here we describe the purpose of our application and how it can benefit users.</p>
+                <footer>
+                    <p>Created by buwu | Hacked by Tahinli</p>
+                </footer>
+            </div>
         </div>
     );
 }
